7a6d42d2efe0c34a3151d8e3e23330f9913eadd1
abhhirramVeedhi <141173291+abhhirramVeedhi@users.noreply.github.com>
2023-11-21 18:14:01 +0530

    Update PrivateRoute.jsx

--- frontend/src/Authentication/PrivateRoute.jsx
+++ frontend/src/Authentication/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import { Outlet, Navigate } from 'react-router-dom';
-import DashboardLayout from '../Dashboard/DashboardLayout';
+import DashboardLayout from '../dashboard/DashboardLayout';
 
 
 const PrivateRoute = () => 
